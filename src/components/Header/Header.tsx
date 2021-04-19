b4fc18d199987b10363da744ff2d86a674b78ebe
pedroksty <pedrotrab2132@gmail.com>
2021-04-19 01:14:01 -0300

    Feat: Create avatar section in Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react";
-import { RiSearchLine  } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine  } from 'react-icons/ri'
 
 
 export function Header() {
@@ -49,6 +49,44 @@ export function Header() {
 
       </Flex>
 
+
+      <Flex
+        align="center"
+        ml="auto"
+      >
+
+        <HStack  
+          spacing="4"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+
+        >
+
+          <Icon  as={RiNotificationLine} fontSize={20} />
+          <Icon  as={RiUserAddLine} fontSize={20} />
+
+        </HStack>
+
+        <Flex align="center" >
+          <Box mr="4" textAlign="right">
+            <Text>
+              Pedro henrique
+            </Text>
+            <Text color="gray.300" fontSize="small">
+              pedro@interas.com.br
+            </Text>
+          </Box>
+
+          <Avatar size="md" name="Pedro henrique" src="https://github.com/pedroksty.png" />
+        </Flex>
+
+
+        
+      </Flex>
     </Flex>
   )
 }
