08326b5a8b3f98a074c6900b7e3be22db52ea2a0
pedroksty <pedrotrab2132@gmail.com>
2021-04-21 15:42:50 -0300

    Feat: Create user pages with pagination component

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,81 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from '@chakra-ui/react'
+import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+import { Header } from '../../components/Header/Header'
+import { Pagination } from '../../components/Pagination/Pagination'
+import { SideBar } from '../../components/SideBar/SideBar'
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto"  px="6">
+
+        <SideBar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800"  p="8"  >
+          <Flex mb="8" justifyContent="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+              
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+
+          <Table colorScheme="whiteAlpha" >
+            <Thead>
+              <Tr>
+                <Th px="6"  color="gray.300" width="8">
+                  <Checkbox colorScheme="pink"/>
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox  colorScheme="pink"/>
+                </Td>
+                <Td >
+                  <Box>
+                    <Text fontWeight="bold" >Pedro henrique</Text>
+                    <Text fontSize="sm" color="gray.300">pedro@interas.com.br</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  04 de Abril, 2021
+                </Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontStyle="16" />}
+                  >
+                Editar
+              </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+
+          <Pagination />
+        </Box>
+
+      </Flex>
+
+    </Box>
+  )
+}
