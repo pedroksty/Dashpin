837d90d3e17e296c969642d298b2428bf3bfd380
pedroksty <pedrotrab2132@gmail.com>
2021-04-24 18:25:49 -0300

    Feat: Add validation to SignIn form data

--- src/components/Form/input.tsx
+++ src/components/Form/input.tsx
@@ -1,13 +1,16 @@
-import { Button, Flex, Input as ChakraInput, Stack, FormLabel, FormControl, InputProps as ChakraInputProps } from '@chakra-ui/react'
+import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps, FormErrorMessage } from '@chakra-ui/react'
+import { forwardRef, ForwardRefRenderFunction } from 'react'
+import { FieldErrors } from 'react-hook-form'
 
 interface InputProps extends ChakraInputProps {
   name: string
   label?: string
+  error?: FieldErrors
 } 
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, error = null, ...rest }, ref) => {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       { !!label && <FormLabel htmlFor={name} >{label}</FormLabel>}
       <ChakraInput
         name={name}
@@ -19,8 +22,19 @@ export function Input({ name, label, ...rest }: InputProps) {
           bgColor: 'gray.900'
         }}
         size="lg"
+        ref={ref}
         {...rest}
         />
+
+        {
+          !!error && (
+            <FormErrorMessage>
+              {error.message}
+            </FormErrorMessage>
+          )
+        }
    </FormControl>
   )
 }
+
+export const Input = forwardRef(InputBase)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,36 @@
 import {Button, Flex, Stack} from '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from "@hookform/resolvers/yup";
+
 import { Input } from '../components/Form/input'
 
-export default function Home() {
+interface SignInFormData {
+  email: string
+  password: string
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória')
+})
+
+export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  })
+
+  const { errors } = formState
+
+  console.log(errors)
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async ({email, password}) => {
+    await new Promise(resolve => setTimeout(resolve, 200))
+    
+    console.log(email, password)
+
+  }
+
   return (
    <Flex
      w="100vw"
@@ -17,13 +46,34 @@ export default function Home() {
        p="8"
        borderRadius={8}
        flexDir="column"
+       onSubmit={handleSubmit(handleSignIn)}
        >
          <Stack  spacing="4" >
-            <Input name="email" label="E-mail" />
-            <Input name="password" type="password" label="Senha"  />
+            <Input
+              name="email"
+              type="email"
+              error={errors.email}
+              label="E-mail"
+              {...register('email')} 
+            />
+            <Input
+              name="password"
+              type="password"
+              label="Senha"
+              error={errors.password}
+              {...register('password')} 
+            />
          </Stack>
 
-          <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>     
+          <Button
+            type="submit"
+            mt="6"
+            colorScheme="pink"
+            size="lg"
+            isLoading={formState.isSubmitting}
+          >
+            Entrar
+          </Button>    
 
       </Flex>
    </Flex>
